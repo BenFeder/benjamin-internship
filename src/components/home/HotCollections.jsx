c1f123ada1d5d325e1cc05a0a7fcc995869942ce
Ben Feder <bballshot90@gmail.com>
2025-12-29 14:07:22 -0500

    Added carousel to nft cards with slider arrows

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,9 +1,116 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+// Custom arrow components
+const NextArrow = (props) => {
+  const { onClick } = props;
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        background: "#fff",
+        borderRadius: "50%",
+        width: "40px",
+        height: "40px",
+        right: "-25px",
+        zIndex: 1,
+        cursor: "pointer",
+        position: "absolute",
+        top: "50%",
+        transform: "translateY(-50%)",
+        border: "1px solid #ddd",
+        textAlign: "center",
+      }}
+      onClick={onClick}
+    >
+      <i 
+        className="fa fa-chevron-right" 
+        style={{ 
+          color: "#555", 
+          fontSize: "14px",
+          margin: "0 auto",
+        }}
+      ></i>
+    </div>
+  );
+};
+
+const PrevArrow = (props) => {
+  const { onClick } = props;
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        background: "#fff",
+        borderRadius: "50%",
+        width: "40px",
+        height: "40px",
+        left: "-25px",
+        zIndex: 1,
+        cursor: "pointer",
+        position: "absolute",
+        top: "50%",
+        transform: "translateY(-50%)",
+        border: "1px solid #ddd",
+        textAlign: "center",
+      }}
+      onClick={onClick}
+    >
+      <i 
+        className="fa fa-chevron-left" 
+        style={{ 
+          color: "#555", 
+          fontSize: "14px",
+          margin: "0 auto",
+        }}
+      ></i>
+    </div>
+  );
+};
 
 const HotCollections = () => {
   const [collections, setCollections] = useState([]);
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
 
   useEffect(() => {
     async function fetchCollections() {
@@ -30,40 +137,41 @@ const HotCollections = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {collections.map((collection) => (
-            <div
-              className="col-lg-3 col-md-6 col-sm-6 col-xs-12"
-              key={collection.id}
-            >
-              <div className="nft_coll">
-                <div className="nft_wrap">
-                  <Link to={`/item-details/${collection.nftId}`}>
-                    <img
-                      src={collection.nftImage}
-                      className="lazy img-fluid"
-                      alt=""
-                    />
-                  </Link>
+          <div className="col-lg-12">
+            <Slider {...settings}>
+              {collections.map((collection) => (
+                <div key={collection.id}>
+                  <div className="nft_coll" style={{ margin: "0 10px" }}>
+                    <div className="nft_wrap">
+                      <Link to={`/item-details/${collection.nftId}`}>
+                        <img
+                          src={collection.nftImage}
+                          className="lazy img-fluid"
+                          alt=""
+                        />
+                      </Link>
+                    </div>
+                    <div className="nft_coll_pp">
+                      <Link to="/author">
+                        <img
+                          className="lazy pp-coll"
+                          src={collection.authorImage}
+                          alt=""
+                        />
+                      </Link>
+                      <i className="fa fa-check"></i>
+                    </div>
+                    <div className="nft_coll_info">
+                      <Link to="/explore">
+                        <h4>{collection.title}</h4>
+                      </Link>
+                      <span>ERC-{collection.code}</span>
+                    </div>
+                  </div>
                 </div>
-                <div className="nft_coll_pp">
-                  <Link to="/author">
-                    <img
-                      className="lazy pp-coll"
-                      src={collection.authorImage}
-                      alt=""
-                    />
-                  </Link>
-                  <i className="fa fa-check"></i>
-                </div>
-                <div className="nft_coll_info">
-                  <Link to="/explore">
-                    <h4>{collection.title}</h4>
-                  </Link>
-                  <span>ERC-{collection.code}</span>
-                </div>
-              </div>
-            </div>
-          ))}
+              ))}
+            </Slider>
+          </div>
         </div>
       </div>
     </section>
